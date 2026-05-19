e74bd3d1aafbdd00d15708cbf69446b5efae5cce
JVMull <21john.vazquez@gmail.com>
2026-05-18 19:34:39 -0500

    Mostly done

--- grad_photo_stealer.py
+++ grad_photo_stealer.py
@@ -1,18 +1,64 @@
 from PIL import Image
 
-img_name = input("Please enter image title:")
-img = Image.open(img_name)
+img_name = "MagnifyRender.ashx"
+alt_img_name = "MagnifyRender.jpeg"
+# input("Please enter image title:")
+
+TRANSPARENT_PIXELS = (0, 0, 0, 0)
+# Open and convert to RGBA Image
+try:
+    img = Image.open(img_name)
+except:
+     print("Different name")
+     img = Image.open(alt_img_name)
+else:
+     print("Image Processed")
+
 rgba = img.convert("RGBA")
-datas = rgba.getdata()
 
-newData = []
-for item in datas:
-    if item[0] == 0 and item[1] == 0 and item[2] == 0:  # finding black colour by its RGB value
-        # storing a transparent value when we find a black colour
-        newData.append((255, 255, 255, 0))
-    else:
-        newData.append(item)  # other colours remain unchanged
 
-rgba.putdata(newData)
-rgba.save("transparent_image.png", "PNG")
-# 33- 35 and 153-155
+with rgba as im:
+    px = im.load()
+    # top bar = x: 33 - 155 y: 33-35
+    for x in range(32, 156 + 1):
+        for y in range(32, 36 + 1):
+            px[x, y] = TRANSPARENT_PIXELS
+    # left bar = x: 33 - 35 y: 36 - 155
+    for x in range(32, 36 + 1):
+            for y in range(35, 156 + 1):
+                px[x, y] = TRANSPARENT_PIXELS
+    # right bar = x: 153 - 155 y: 36 - 155   
+    for x in range(152, 156 + 1):
+            for y in range(35, 156 + 1):
+                px[x, y] = TRANSPARENT_PIXELS
+    # bottom bar = x: 36 - 152 y: 153 - 155
+    for x in range(35, 153 + 1):
+            for y in range(152, 156 + 1):
+                px[x, y] = TRANSPARENT_PIXELS
+im.save('output.png')
+
+
+
+#  class PixelAccess
+
+#     __getitem__(self, xy: tuple[int, int]) → float | tuple[int, ...]
+
+#         Returns the pixel at x,y. The pixel is returned as a single value for single band images or a tuple for multi-band images.
+
+#         Parameters:
+
+#             xy – The pixel coordinate, given as (x, y).
+#         Returns:
+
+#             a pixel value for single band images, a tuple of pixel values for multiband images.
+
+#     __setitem__(self, xy: tuple[int, int], color: float | tuple[int, ...]) → None
+
+#         Modifies the pixel at x,y. The color is given as a single numerical value for single band images, and a tuple for multi-band images. See Colors for more information.
+
+#         Parameters:
+
+#                 xy – The pixel coordinate, given as (x, y).
+
+#                 color – The pixel value according to its mode, e.g. tuple (r, g, b) for RGB mode.
+
